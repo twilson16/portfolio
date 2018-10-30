8cf99b08bc84ae08e7f4d21c85fe5ad6d6f12a83
Tina <christinawilson6@gmail.com>
2018-10-30 16:12:12 -0400

    Updates

---
+++ precache-manifest.50ab14929f9f829b718597ed106e9853.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "529819f4652661202e9f",
+    "url": "/portfolio/static/js/main.529819f4.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/portfolio/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "529819f4652661202e9f",
+    "url": "/portfolio/static/css/main.eab137a2.chunk.css"
+  },
+  {
+    "revision": "74e001bd537e5e1e02bb70341718efd3",
+    "url": "/portfolio/index.html"
+  }
+];

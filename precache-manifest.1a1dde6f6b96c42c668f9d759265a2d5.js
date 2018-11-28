f613fa1fbc663ec92aaddf3b91a2749090ce05b7
Tina <christinawilson6@gmail.com>
2018-11-28 16:07:17 -0500

    Updates

---
+++ precache-manifest.1a1dde6f6b96c42c668f9d759265a2d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cb18269c8d95c5ad0e005b7d659845e",
+    "url": "/static/media/square-game.0cb18269.png"
+  },
+  {
+    "revision": "578d66fea3f470f78f4d25cdfc72741a",
+    "url": "/static/media/qr-scan.578d66fe.png"
+  },
+  {
+    "revision": "498628a46e633b4897ab84e8064fba56",
+    "url": "/static/media/cropped_misery_ridge_adj.498628a4.jpg"
+  },
+  {
+    "revision": "76b4a9271168b5dccf19d3bcf48fb262",
+    "url": "/static/media/ChristinaWilsonResume2018.76b4a927.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff3f9029f3713d7c02e5",
+    "url": "/static/js/main.ff3f9029.chunk.js"
+  },
+  {
+    "revision": "8dfcd3645d0543189a7d",
+    "url": "/static/js/1.8dfcd364.chunk.js"
+  },
+  {
+    "revision": "ff3f9029f3713d7c02e5",
+    "url": "/static/css/main.ad0e3c7f.chunk.css"
+  },
+  {
+    "revision": "4bfa8d38c85840ad0608be6e661d26f0",
+    "url": "/index.html"
+  }
+];

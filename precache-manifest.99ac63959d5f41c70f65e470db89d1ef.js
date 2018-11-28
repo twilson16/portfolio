e8f04e69dda1a7dabe292958882d6897fb9b4fa5
Tina <christinawilson6@gmail.com>
2018-11-28 15:19:03 -0500

    Updates

---
+++ precache-manifest.99ac63959d5f41c70f65e470db89d1ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4af3000075592e16d59139fbc1d0f75a",
+    "url": "/portfolio/static/media/resize-watson-falls.4af30000.jpg"
+  },
+  {
+    "revision": "498628a46e633b4897ab84e8064fba56",
+    "url": "/portfolio/static/media/cropped_misery_ridge_adj.498628a4.jpg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "417ec53dc8bf32872ccb",
+    "url": "/portfolio/static/js/main.417ec53d.chunk.js"
+  },
+  {
+    "revision": "6458edbe65a357a71bfd",
+    "url": "/portfolio/static/js/1.6458edbe.chunk.js"
+  },
+  {
+    "revision": "417ec53dc8bf32872ccb",
+    "url": "/portfolio/static/css/main.7a27ce69.chunk.css"
+  },
+  {
+    "revision": "5eb249e263c1b0cf7324488b85bb3da0",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c942795396b10ff80c424208adcdda8c.js"
+  "/portfolio/precache-manifest.99ac63959d5f41c70f65e470db89d1ef.js"
 );
 
 workbox.clientsClaim();

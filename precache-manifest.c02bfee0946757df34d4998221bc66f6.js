804aa3ad5e6d22b092d2055a2652d7d5d6a32f33
Tina <christinawilson6@gmail.com>
2019-01-10 15:29:30 -0500

    Updates

---
+++ precache-manifest.c02bfee0946757df34d4998221bc66f6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cb18269c8d95c5ad0e005b7d659845e",
+    "url": "/portfolio/static/media/square-game.0cb18269.png"
+  },
+  {
+    "revision": "578d66fea3f470f78f4d25cdfc72741a",
+    "url": "/portfolio/static/media/qr-scan.578d66fe.png"
+  },
+  {
+    "revision": "498628a46e633b4897ab84e8064fba56",
+    "url": "/portfolio/static/media/cropped_misery_ridge_adj.498628a4.jpg"
+  },
+  {
+    "revision": "76b4a9271168b5dccf19d3bcf48fb262",
+    "url": "/portfolio/static/media/ChristinaWilsonResume2018.76b4a927.png"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "eda3f4dd84c21479e495",
+    "url": "/portfolio/static/js/main.eda3f4dd.chunk.js"
+  },
+  {
+    "revision": "650dc98cd36b72938fec",
+    "url": "/portfolio/static/js/1.650dc98c.chunk.js"
+  },
+  {
+    "revision": "eda3f4dd84c21479e495",
+    "url": "/portfolio/static/css/main.ad0e3c7f.chunk.css"
+  },
+  {
+    "revision": "c5289f12f983248395ca6f413efb7185",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c25ed6d0c97281a131f01c27f639b449.js"
+  "/portfolio/precache-manifest.c02bfee0946757df34d4998221bc66f6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
